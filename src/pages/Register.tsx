0d0e5693065f275f5263a5dbeb4f153ab7042ddd
ShabahDev <osameh.asakereh@chasacademy.se>
2026-03-11 10:49:48 +0100

    Register, Login Page CSS Fixed

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -3,18 +3,20 @@ import "./Register.css";
 export function RegisterPage() {
   return (
     <main className="register-page">
-      <h1>Register</h1>
-      <p>This is the register page.</p>
-      <form className="register-form" action="">
-        <p>Email</p>
-        <input type="email" placeholder="example@gmail.com" />
-        <p>Password</p>
-        <input type="password" placeholder="password" />
-      </form>
-      <button className="register-button">Create account</button>
-      <p>
-        Already have an account? <a href="/login">Go to login</a>
-      </p>
+      <div className="form-wrapper">
+        <h1>Register</h1>
+        <p className="subtitle">This is the register page.</p>
+        <form className="register-form" action="">
+          <p>Email</p>
+          <input type="email" placeholder="example@gmail.com" />
+          <p>Password</p>
+          <input type="password" placeholder="password" />
+        </form>
+        <button className="register-button">Create account</button>
+        <p className="footer-text">
+          Already have an account? <a href="/login">Go to login</a>
+        </p>
+      </div>
     </main>
   );
 }
